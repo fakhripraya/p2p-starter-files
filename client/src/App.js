b615ed8ba60b7662854e5c23a6781dcc22c6ea43
fakhripraya <fakhri@indomaret.co.id>
2022-11-13 13:16:08 +0700

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -52,16 +52,56 @@ function App() {
     })
 
     socket.current.on("hey", (data) => {
-     
+      setReceivingCall(true);
+      setCaller(data.from);
+      setCallerSignal(data.signal);
     })
   }, []);
 
   function callPeer(id) {
-    
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream
+    });
+
+    peer.on("signal", data => {
+      socket.current.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: yourID
+      })
+    });
+
+    peer.on("stream", stream => {
+      if (partnerVideo.current) {
+        partnerVideo.current.srcObject = stream;
+      }
+    });
+
+    socket.current.on("callAccepted", signal => {
+      setCallAccepted(true);
+      peer.signal(signal)
+    });
   }
 
   function acceptCall() {
-    
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on("signal", data => {
+      socket.current.emit("acceptCall", { signal: data, to: caller });
+    })
+
+    peer.on("stream", stream => {
+      partnerVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
   }
 
   let UserVideo;
@@ -99,7 +139,7 @@ function App() {
             return null;
           }
           return (
-            <button onClick={() => callPeer(key)}>Call {key}</button>
+            <button key={key} onClick={() => callPeer(key)}>Call {key}</button>
           );
         })}
       </Row>
